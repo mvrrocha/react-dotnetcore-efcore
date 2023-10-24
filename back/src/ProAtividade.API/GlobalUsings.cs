dce953cb4938356381f70e2be5703755b932888b
mvrrocha <marcusrocharj@yahoo.com.br>
2023-10-23 22:40:30 -0300

    Secao 8 - NET 5 para NET 6

---
+++ back/src/ProAtividade.API/GlobalUsings.cs
@@ -0,0 +1,13 @@
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.OpenApi.Models;
+global using Microsoft.EntityFrameworkCore;
+global using System.Text.Json.Serialization;
+global using ProAtividade.Data.Context;
+global using ProAtividade.Domain.Interfaces.Repositories;
+global using ProAtividade.Data.Repositories;
+global using ProAtividade.Domain.Services;
+global using ProAtividade.Domain.Interfaces.Services;
